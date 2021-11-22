0f49d6d1b6d691bf4e41fa788110f6fc0ffd45b3
Siddhant <siddhantsharma669@gmail.com>
2021-11-22 12:57:50 +0530

    This file contain the  javascript file associated with the index.html file

---
+++ js/js_introduction/htmlScript.js
@@ -0,0 +1,4 @@
+console.log("Hello from the NODE");
+//Javascript code runs top to buttom and left to right
+// TO run this js file through node we will simply type in the terminal - node <filename>
+
