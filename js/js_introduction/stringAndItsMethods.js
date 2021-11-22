cd4932f3c42b463be47185f4fcc6ebda769741ee
Siddhant <siddhantsharma669@gmail.com>
2021-11-22 13:01:11 +0530

    various operation/methods that could be performed on a string  in javascript

---
+++ js/js_introduction/stringAndItsMethods.js
@@ -0,0 +1,59 @@
+
+// ***********************************************STRING*******************************************************
+
+
+//STRING METHODS 
+
+// 1.> length
+// 2.> extracting - slice , substring
+// 3.> toupper , tolower 
+// 4.> replace
+// 5.> concat
+// 6.> trim
+
+let str = "pepcoders";
+console.log(str);
+
+//STRING METHOD
+
+//To determine length
+// .length
+console.log(str.length);
+
+// Extracting the part of string - .slice , .substr
+
+let slicedStr = str.slice(3, 7);//here 7 is not been included therfore last index is excluded
+console.log(slicedStr);
+
+
+let subString = str.substr( 3, 5);// here 3 is the starting index and 5 is the number of elements we to trvel from 3
+console.log(subString);
+
+//replace method for string 
+let sayHello = "Hello mukesh";
+let saybye = sayHello.replace('Hello', 'bye');//The word you need to replace and which word would you replace it with
+
+console.log(saybye);
+
+//To upper case or lower case
+let text1 = "hello world" ;
+let text2 = text1.toUpperCase();
+let text3 = text1.toLowerCase();
+console.log(text1);
+console.log(text2);
+console.log(text3);
+
+//concatenation of string
+
+let fristStr = "Hello 1";
+let secondStr = "World 1";
+let concatenatedstr = fristStr.concat(secondStr);
+console.log(concatenatedstr);
+
+//Trim method 
+
+let text = "      hello 2 world 2        ";
+console.log(text);
+let trimmedText = text.trim();
+console.log(trimmedText);
+
