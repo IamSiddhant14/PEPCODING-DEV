3872818acbac023e24a8abe30d60079caec3e326
Siddhant <siddhantsharma669@gmail.com>
2021-12-20 12:39:56 +0530

    changed folder structure

--- js/1.js_introduction/2htmlScript.js
+++ js/1.js_introduction/2htmlScript.js
@@ -1,4 +1,4 @@
-console.log("Hello from the NODE");
+console.log("Hello from NODE");
 //Javascript code runs top to buttom and left to right
 // TO run this js file through node we will simply type in the terminal - node <filename>
 
