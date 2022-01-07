6a2bae73a2663c44162a60139145d97e3c4d0394
Siddhant <siddhantsharma669@gmail.com>
2022-01-08 02:33:15 +0530

    os module

---
+++ JAVASCRIPT/Node/2_os.js
@@ -0,0 +1,9 @@
+const os = require('os');
+
+console.log(os.arch());//Architecture 64 0r 32
+
+console.log( os.platform()); //Tells about platform
+
+console.log(os.networkInterfaces()); //give details regarding network
+
+console.log(os.cpus()); //Cpu specification
