e7df4ade702e807760eb91266d3dbb8771b10d09
Siddhant <siddhantsharma669@gmail.com>
2022-01-09 07:58:57 +0530

    os module

--- JAVASCRIPT/Node/2_os.js
+++ JAVASCRIPT/Node/2_os.js
@@ -1,9 +1,11 @@
+//os module gives us the info related to our os and cpu
 const os = require('os');
 
 console.log(os.arch());//Architecture 64 0r 32
 
-console.log( os.platform()); //Tells about platform
+console.log(os.platform()); //Tells about platform
 
 console.log(os.networkInterfaces()); //give details regarding network
 
 console.log(os.cpus()); //Cpu specification
+
