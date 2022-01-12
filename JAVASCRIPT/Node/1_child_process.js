75244998c02c0819e9dca276236926d73af14b55
Siddhant <siddhantsharma669@gmail.com>
2022-01-12 16:58:30 +0530

    Added comments

--- JAVASCRIPT/Node/1_child_process.js
+++ JAVASCRIPT/Node/1_child_process.js
@@ -1,3 +1,4 @@
+// To run a js file using nodejs we use in integrated terminal simply type -- node <filename.js>
 //NPM - node package manager is like a play store to the various node packges avaliable in node module . 
 const cp = require('child_process');//This is an node module which help us in creating various sub process within a script
 //You can perfome diffrent task using this script
@@ -15,4 +16,5 @@ cp.execSync('start chrome https://github.com/IamSiddhant14');//Open browser chro
 
 let output = cp.execSync('node test.js');
 
-console.log( 'output ->' + output);//WHen data is returned in the format of binary then we perfome conatenation
+console.log( 'output ->' + output);//WHen data is returned in the format of binary then we perfome conatenation to convert it into normal text
+console.log( 'output ->', output);//This wont work here and would result in unwated output
