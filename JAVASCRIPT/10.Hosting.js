7b76db29a1ca1fd81735cba3d114b51927ad6a01
Siddhant <siddhantsharma669@gmail.com>
2022-01-25 19:20:24 +0530

    Hosting file added

--- JAVASCRIPT/10.Hosting.js
+++ JAVASCRIPT/10.Hosting.js
@@ -1,4 +1,7 @@
 // ***********************************************************************
+//Unlike var, let and const variables are not hoisted in the global space instead and also we could not access that memory space until we have assigned some value to it
+
+//The space bewteen when the variable let is been hoisted and till when it is been assigned some value is called an temporal deadzone so whenever we try to access a variable bewteen it ,then it would result in an reffrence error
 //NORMALE CASE FOR VAR
 
 var a = 2
@@ -39,7 +42,11 @@ function greet(){
 // ***********************************************************************
 //HOISTING CASE FOR LET
 
-// TEMPORAL DEAD ZONE - it is just a area where if you try to access variables defined with let and const before their initialization you wont be able to do it.
+// TEMPORAL DEAD ZONE - It is an zone where if you try to access variables defined with let and const before their initialization you wont be able to do it and would result in an reffrence error.
+
+//Unlike var, let and const variables are not hoisted in the global space instead and also we could not access that memory space until we have assigned some value to it
+
+//The space bewteen when the variable let is been hoisted and till when it is been assigned some value is called an temporal deadzone so whenever we try to access a variable bewteen it ,then it would result in an reffrence error
 
 //This would result in an initalization error as due to hosting we will be assining "let a "as " undefined" and since in" let" or "const" redeclaring the same variable isnt possible Therfore it would result in an error for that matter
 
