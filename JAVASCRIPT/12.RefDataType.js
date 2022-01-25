2166afa42f166b2d37d2102e0b6be9a157d45d89
Siddhant <siddhantsharma669@gmail.com>
2022-01-25 19:20:52 +0530

    Refrence datatype in javacript

---
+++ JAVASCRIPT/12.RefDataType.js
@@ -0,0 +1,25 @@
+let arr = [1 , 3 , 3]
+
+console.log(arr , typeof(arr))
+
+
+let obj = {
+       name : 'Adam',
+       age :23
+}
+
+console.log(obj , typeof(obj))
+
+
+
+function greet(){
+       console.log('Hello')
+}
+
+console.log(greet , typeof(greet))
+
+// Output
+
+// [ 1, 3, 3 ] object
+// { name: 'Adam', age: 23 } object
+// [Function: greet] function
