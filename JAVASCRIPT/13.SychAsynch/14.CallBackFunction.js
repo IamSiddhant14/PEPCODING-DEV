1abf5c1a854fd5802c254295f6e0af99e80a6498
Siddhant <siddhantsharma669@gmail.com>
2022-01-27 23:51:50 +0530

    call back fuction

---
+++ JAVASCRIPT/13.SychAsynch/14.CallBackFunction.js
@@ -0,0 +1,26 @@
+// Callback function - any function that is passed as an
+//argument to another function is known
+//as a callback function
+
+function printFristName(fristName , cb , cb1){
+
+    console.log(fristName);
+
+    cb("Sharma");
+    cb1(21);
+
+}
+
+function printLastName(lastname){
+    console.log(lastname);
+}
+
+function printAge(age){
+    console.log(age);
+}
+
+
+
+printFristName("Siddhant" , printLastName , printAge);
+// Here we are passing the entire function body as a argument then we are passing the parameter to that function which is been passed
+
