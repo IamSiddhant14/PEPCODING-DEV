9977063aaa3ac169b4b1d3cf9bf40e7d9257abb6
Siddhant <siddhantsharma669@gmail.com>
2022-02-07 15:56:47 +0530

    Name changed

--- JAVASCRIPT/14.Higher_Order_Function/intro.js
+++
@@ -1,5 +0,0 @@
-// Higher order function are thoughs functions which operates on other function
-
-// In simple words, A Higher-Order function is a function that receives a function as an argument
-// or returns the function as output.
-
