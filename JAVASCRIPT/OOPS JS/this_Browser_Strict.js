d1a680b2e3735be93d1a57da57f2944cfdba70d2
Siddhant <siddhantsharma669@gmail.com>
2022-02-23 16:53:35 +0530

    browser strict mode

---
+++ JAVASCRIPT/OOPS JS/this_Browser_Strict.js
@@ -0,0 +1,52 @@
+//Window object carries all the bowser methods
+
+//********************************************************
+
+//console.log(this) // It will result in window objectwindow Object
+
+//********************************************************
+
+// function f(){
+//        console.log(this)
+// }
+
+// f() //Undefined
+
+// 
+
+
+//********************************************************
+
+// let obj = {
+//     f: function(){
+//            console.log(this)
+//     }
+// }
+
+// obj.f() //This will result in object itself 
+
+
+// let obj = {
+//        f: function(){
+//               function g(){
+//                      console.log(this)
+//               }
+//               g()
+//        }
+// }
+
+// obj.f() //This will result window object
+
+
+
+//Sumarry -
+
+//Context : Browser +strict ->
+
+// global area -> Window Object
+
+// function - > Undefined
+
+// obj -> function - object Itself
+
+// obj - function - function -  Undefined
