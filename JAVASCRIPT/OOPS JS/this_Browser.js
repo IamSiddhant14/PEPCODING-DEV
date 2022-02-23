4090de7060815b562fefa83baab70686112947e3
Siddhant <siddhantsharma669@gmail.com>
2022-02-23 16:54:23 +0530

    browser

---
+++ JAVASCRIPT/OOPS JS/this_Browser.js
@@ -0,0 +1,54 @@
+//Window object carries all the bowser methods
+
+
+
+//********************************************************
+
+//console.log(this) // It will result in window objectwindow Object
+
+//********************************************************
+
+// function f(){
+//        console.log(this)
+// }
+
+// f() //It will result in window object window Object
+
+// 
+
+
+//********************************************************
+
+// let obj = {
+//     f: function(){
+//            console.log(this)
+//     }
+// }
+
+// obj.f() //This will result in object itself 
+
+
+// let obj = {
+//        f: function(){
+//               function g(){
+//                      console.log(this)
+//               }
+//               g()
+//        }
+// }
+
+// obj.f() //This will result window object
+
+
+
+//Sumarry -
+
+//Context : Browser + non strict ->
+
+// global area -> Window Object
+
+// function - > Window Object
+
+// obj -> function - object Itself
+
+// obj - function - function -  Window Object
