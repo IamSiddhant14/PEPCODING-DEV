c2920079ca2dfc159cab6ad0b93dfc12691479a6
Siddhant <siddhantsharma669@gmail.com>
2022-02-23 16:54:31 +0530

    node unstrict

--- JAVASCRIPT/OOPS JS/this.js
+++ JAVASCRIPT/OOPS JS/this.js
@@ -5,13 +5,21 @@
 
 //console.log(this) // global area  // Empty Object
 
+
+// ********************************************************
+
+
 // inside a function
 
 // function f(){
-//        console.log(this)  //THIS WILL RESULT IN AN GLOBAL OBJECT
+//        console.log(this)  
+
+//THIS WILL RESULT IN AN GLOBAL OBJECT( Global obj != Window Object as both have diffrent poles apart property in node we have global object and not window object)
 // }
 
 // f()
+
+// ********************************************************
 
 
 // inside a function inside an  Object
@@ -20,6 +28,38 @@
 //    name : 'rahul',
 
 //    f:function(){
-//          console.log(this)
+//          console.log(this) 
 //    }
 // }
+
+// obj.f() //It will return (obj) object itself.
+
+
+// ********************************************************
+
+
+// let obj2 ={
+//     name :'Rahul',
+//     f : function(){
+//            function g(){
+//                   console.log(this)
+//            }
+//            g()
+//     }
+    
+// }
+
+// obj2.f() //THIS WILL RESULT IN AN GLOBAL OBJECT
+
+
+//Sumarry -
+
+//Context : node + non strict ->
+
+// global area -> empty Object
+
+// function - > global Object
+
+// obj -> function - object Itself
+
+// obj - function - function -  global Object
