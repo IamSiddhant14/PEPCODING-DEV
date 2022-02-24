a9be089667bc34e183b92a1c4f0ec3d4a62e839b
Siddhant <siddhantsharma669@gmail.com>
2022-02-24 23:03:50 +0530

    completed promises

--- JAVASCRIPT/13.SychAsynch/Promises/readingFilesSeriallyWithPromises.js
+++ JAVASCRIPT/13.SychAsynch/Promises/readingFilesSeriallyWithPromises.js
@@ -1,23 +1,52 @@
+// const fs = require('fs');
+
+// let f1p = fs.promises.readFile('./f1.txt');
+
+// f1p.then(cb);
+
+// function cb(data){
+
+//     console.log('File data ->' + data);
+//     let f2p = fs.promises.readFile('./f2.txt');
+
+//     f2p.then(cb2);
+// }
+
+// function cb2 (data){
+
+//     console.log("File data -->"+ data);
+//     let f3p = fs.promises.readFile('./f2.txt');
+
+//     f3p.then(cb3);
+     
+
+// }
+
+// function cb3 (data){
+
+//     console.log("File data -->"+ data); 
+// }
+
+
+
 const fs = require('fs');
 
 let f1p = fs.promises.readFile('./f1.txt');
 
-f1p.then(cb);
-
-function cb(data){
+function cb1(data){
 
     console.log('File data ->' + data);
     let f2p = fs.promises.readFile('./f2.txt');
 
-    f2p.then(cb2);
+    return f2p;
 }
 
 function cb2 (data){
 
     console.log("File data -->"+ data);
-    let f3p = fs.promises.readFile('./f2.txt');
+    let f3p = fs.promises.readFile('./f3.txt');
 
-    f3p.then(cb3);
+    return f3p;
      
 
 }
@@ -25,6 +54,9 @@ function cb2 (data){
 function cb3 (data){
 
     console.log("File data -->"+ data); 
+    
 }
+
+f1p.then(cb1).then(cb2).then(cb3)
 
 
