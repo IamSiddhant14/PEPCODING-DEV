7f2256b66d0fe0de6921a6373055d00ee81fde12
Siddhant <siddhantsharma669@gmail.com>
2022-03-19 10:38:55 +0530

    Added

--- JAVASCRIPT/OOPS In JS/thisNodeSTRICT.js
+++ JAVASCRIPT/OOPS In JS/thisNodeSTRICT.js
@@ -1,45 +1,45 @@
 // ************ Node -> Strict Mode (REACT)
 'use strict'
 
-// console.log(this) // global area  // Empty Object
+console.log(this) // global area  // Empty Object
 
 // ********************************************************
 
 
-// function f(){
-//        console.log(this)
-// }
+function f(){
+       console.log(this)
+}
 
-// f() // Undefined
-
-// ********************************************************
-
-// let obj = {
-//        name : 'Rahul',
-
-//        f: function(){
-//               console.log(this)
-//        }
-// }
-
-// obj.f()  // object (obj) itself
+f() // Undefined
 
 // ********************************************************
 
-// let obj2 = {
-//        name :'Mrinal',
+let obj = {
+       name : 'Rahul',
 
-//        f: function f(){
-//               function g(){
-//                      console.log(this)
-//               }
+       f: function(){
+              console.log(this)
+       }
+}
 
-//               g();
-//        }
-// }
+obj.f()  // object (obj) itself
+
+// ********************************************************
+
+let obj2 = {
+       name :'Mrinal',
+
+       f: function f(){
+              function g(){
+                     console.log(this)
+              }
+
+              g();
+       }
+}
 
 
-// obj2.f()  //This will result in undefined
+obj2.f()  //This will result in undefined
 
 
 //Sumarry -
