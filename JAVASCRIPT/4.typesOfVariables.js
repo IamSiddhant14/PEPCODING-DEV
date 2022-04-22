1c4cec44d5d803b4eaf47b586cfabd35831bf25a
Siddhant <siddhantsharma669@gmail.com>
2022-04-23 00:59:55 +0530

    var is function scoped ,

--- JAVASCRIPT/4.typesOfVariables.js
+++ JAVASCRIPT/4.typesOfVariables.js
@@ -24,76 +24,76 @@
 
 //variable declartion with let var and const
 
-var a ; // frist the variable is been initalzed with undefined
-console.log(a);
-a=20;
-console.log(a);
+// var a ; // frist the variable is been initalzed with undefined
+// console.log(a);
+// a=20;
+// console.log(a);
 
-a ="20";
-console.log(a);
-// a="Hello"; dynamic behaviour of javascript
-console.log(a);
-a= true;
-console.log(a);
-// assigning value to "a" as null
-a= null;
-console.log(a);
+// a ="20";
+// console.log(a);
+// // a="Hello"; dynamic behaviour of javascript
+// console.log(a);
+// a= true;
+// console.log(a);
+// // assigning value to "a" as null
+// a= null;
+// console.log(a);
 
-//javascript is an dyanimically typed language that is dont need to specifie the type of variable as such
-// semicolon is not compulsory in javascript
-// Javascript is a single threaded language not multithreaded 
-//javascript is a sychronus language by default.
+// //javascript is an dyanimically typed language that is dont need to specifie the type of variable as such
+// // semicolon is not compulsory in javascript
+// // Javascript is a single threaded language not multithreaded 
+// //javascript is a sychronus language by default.
 
-//let and const :-
+// //let and const :-
 
-//frist problem of var : redeclartion
-//That is 
-var a = 2;
-console.log(a);
-var a = "string";
-console.log(a);
+// //frist problem of var : redeclartion
+// //That is 
+// var a = 2;
+// console.log(a);
+// var a = "string";
+// console.log(a);
 
 
 
-// But in case of "let" keyword we could not redeclare the same variable where was this was allowed in var
-//but here re-defining is allowed , same as that in var keyword javascript
+// // But in case of "let" keyword we could not redeclare the same variable where was this was allowed in var
+// //but here re-defining is allowed , same as that in var keyword javascript
 
-//redeclaring(not allowed) will result in an syantax error and redefing/reassigning (allowed) this two are two diffrent things
+// //redeclaring(not allowed) will result in an syantax error and redefing/reassigning (allowed) this two are two diffrent things
 
-let b = 2;
-console.log(b);
-
-//This will result in an error
-// let b = "hello";
+// let b = 2;
 // console.log(b);
 
-// This is possible in case of let and var 
-b=4; //redefining
-console.log(b);
+// //This will result in an error
+// // let b = "hello";
+// // console.log(b);
 
-//Whether the number is prime or not
-let flag = true;
-let num = 13;
+// // This is possible in case of let and var 
+// b=4; //redefining
+// console.log(b);
 
-let a;
+// //Whether the number is prime or not
+// let flag = true;
+// let num = 13;
 
-a= 10;//Although this is possible 
+// let a;
+
+// a= 10;//Although this is possible 
 
 
 
-for( let i =2 ; i*i < num ; i++){//In loop we should not use "var" keyword instead we should use "let "as its value is saved for a particular iteration
-    //"let" has block scope that is it have exsitance within the block only unlike "var " which has function scope
-    if( num % i == 0){
-        flag = false;
-        break;
-    }
-}
+// for( let i =2 ; i*i < num ; i++){//In loop we should not use "var" keyword instead we should use "let "as its value is saved for a particular iteration
+//     //"let" has block scope that is it have exsitance within the block only unlike "var " which has function scope
+//     if( num % i == 0){
+//         flag = false;
+//         break;
+//     }
+// }
 
-if( flag == true){
-    console.log(num +"is prime");
-}else{
-    console.log(num + "is not prime")
-}
+// if( flag == true){
+//     console.log(num +"is prime");
+// }else{
+//     console.log(num + "is not prime")
+// }
 
 //problem -2 with var :
 
@@ -111,9 +111,10 @@ console.log(c); //no error , only the let and const variables are block scoped a
 if( 10%2 ==0){
 // Here this k doesnt exist after this block
     let k = 1;
-    console.log(k);
+    console.log(k+"This one ");
 }
-// console.log(k); This 'let' variable will result in an error but in case of 'var' this wont result in an error
+console.log(k); 
+// This 'let' variable will result in an error but in case of 'var' this wont result in an error
 
 // I hereby plege to use "let" only and not "var" because of the following problems (Will get the refrens later to this sentence)
 
@@ -137,4 +138,23 @@ console.log("2",d);
 console.log("2 "+d);
 
 
+//var is function scoped , try the below example
 
+
+var a = 10;
+function c(){
+    var a = 30;
+    console.log(a);
+}
+c();
+console.log(a);
+
+
+
+var a = 10;
+if(true){
+    var a = 10;
+    console.log(a);
+}
+
+console.log(a);
